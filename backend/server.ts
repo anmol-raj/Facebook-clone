d54043979b38ecaab3ae6ed8580b6238252e4160
Anmol Raj <anmol@tykeinvest.com>
2023-04-22 18:47:34 +0530

    initial commit for fb-clone..

---
+++ backend/server.ts
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('welcome from home')
+});
+
+app.get('/books', (req, res) => {
+    res.send("Welcome from books")
+})
+
+app.listen(8000, () => {
+    console.log("server is listening");
+});
