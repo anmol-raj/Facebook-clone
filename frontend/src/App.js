49b0e19c2105d9fb4303220176e1ce7b7973505f
Anmol Raj <anmol@tykeinvest.com>
2023-06-17 21:01:10 +0530

    frontend setup onprogress.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Profile from "./pages/profile";
+
 function App() {
-  const get = async () => {
-    const res = await fetch("http://localhost:8000");
-    console.log(res);
-  };
-  get();
-  return <div>welcome to frontend</div>;
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} exact />
+      <Route path="/profile" element={<Profile />} exact />
+      <Route path="/" element={<Home />} exact />
+    </Routes>
+  );
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "./styles/icons/icons.css";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import rootReducer from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { Provider } from "react-redux";
+import { legacy_createStore as createStore } from "redux";
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
