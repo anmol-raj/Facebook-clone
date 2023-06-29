031b61b4e7a1e0e812c35fa00763735db6360df5
Anmol Raj <anmol@tykeinvest.com>
2023-06-29 17:34:51 +0530

    HashRouter

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "./styles/icons/icons.css";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import rootReducer from "./reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { Provider } from "react-redux";
