b837848c81f54d18dffe94a284c9e0fb9c49ee71
Anmol Raj <anmol@tykeinvest.com>
2023-07-04 18:14:36 +0530

    fixed issue with home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Login from "./pages/login";
 import Profile from "./pages/profile";
 import Home from "./pages/home";
@@ -8,7 +8,15 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} exact />
         <Route path="/profile" element={<Profile />} exact />
-        <Route path="/" element={<Home />} exact />
+        <Route
+          path="/"
+          element={
+            <h1>
+              HomePage <Link to="/login"> Login page</Link>
+            </h1>
+          }
+          exact
+        />
       </Routes>
     </div>
   );
